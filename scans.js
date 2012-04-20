62489b64ec69207fe7cce68a8cfa52a67d3e117f
Prashant Singh <prashant@codeforamerica.org>
2012-04-19 20:21:58 -0700

    Fix S3 and scanned image timing issue
    Don't add image data to the DB until after we've stored the image on S3.

--- scans.js
+++ scans.js
@@ -83,52 +83,50 @@ function setup(app, db, idgen, collectionName) {
       status: STATUS_PENDING
     };
 
-    // Add image info to the database.
     getCollection(function(err, collection) {
-      collection.insert(data, function() {
-        // Store the image data in buffers
-        req.on('data', function(data) {
-          buffers.push(data);
+      // Store the image data in buffers
+      req.on('data', function(data) {
+        buffers.push(data);
+      });
+
+      // When the upload has finished, we can figure out the content length
+      // and send to Amazon.
+      // TODO: use S3 Multipart uploads so we don't have to keep the whole
+      // file around in Buffer objects.
+      req.on('end', function() {
+        var contentLength = buffers.reduce(function(len,el) { return len + el.length; }, 0);
+        var s3request = s3client.put(makeS3Location(id), {
+          'Content-Length': contentLength,
+          'Content-Type': data.mimetype
         });
 
-        // When the upload has finished, we can figure out the content length
-        // and send to Amazon.
-        // TODO: use S3 Multipart uploads so we don't have to keep the whole
-        // file around in Buffer objects.
-        req.on('end', function() {
-          var contentLength = buffers.reduce(function(len,el) { return len + el.length; }, 0);
-          var s3request = s3client.put(makeS3Location(id), {
-            'Content-Length': contentLength,
-            'Content-Type': data.mimetype
-          });
+        // When we receive the S3 response, we're done.
+        s3request.on('response', function(res) {
+          res
+          .on('data', function(chunk) { console.log(chunk.toString()); })
+          .on('close', function(error) { console.log(error.message); })
+          .on('end', function() {
+            // TODO: return the DB doc instead?
+            body = JSON.stringify({success: 'true', name: [UPLOAD_DIR, filename].join('/')});
 
-          // When we receive the S3 response, we're done.
-          s3request.on('response', function(res) {
-            res
-            .on('data', function(chunk) {
-              console.log(chunk.toString());
-            })
-            .on('close', function(error) {
-              console.log(error.message);
-            })
-            .on('end', function() {
-              // TODO: return the DB doc instead?
-              body = JSON.stringify({success: 'true', name: [UPLOAD_DIR, filename].join('/')});
+            // Add image info to the database.
+            collection.insert(data, function() {
               response.end(body);
               console.log('Added file info:');
               console.log(JSON.stringify(data, null, '  '));
+
               // Track that we have pending scans
               workChecker.gotWork();
             });
           });
-
-          // Write to the S3 request.
-          for (var i=0; i<buffers.length; i++) {
-            console.log('Writing chunk ' + i + ' of ' + buffers.length + ' to S3.');
-            s3request.write(buffers[i]);
-          }
-          s3request.end();
         });
+
+        // Write to the S3 request.
+        for (var i=0; i<buffers.length; i++) {
+          console.log('Writing chunk ' + i + ' of ' + buffers.length + ' to S3.');
+          s3request.write(buffers[i]);
+        }
+        s3request.end();
       });
     });
   });
