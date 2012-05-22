2c5917b80695f33248cd040b1d2459d525aab1f4
Prashant Singh <prashant@codeforamerica.org>
2012-05-21 23:00:47 -0700

    Reenable Access-Control-Allow-Headers header

--- web.js
+++ web.js
@@ -35,7 +35,7 @@ app.configure(function() {
   // Add common headers
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Mime-Type, X-Requested-With, X-File-Name, Content-Type");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Mime-Type, X-Requested-With, X-File-Name, Content-Type");
     next();
   });
 
