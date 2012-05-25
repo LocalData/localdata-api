e50f1c132e7de25ea585d5c4f8c3df6461973b97
hampelm <hampelm>
2012-05-24 22:49:18 -0700

    First tests for commasep

--- test/test.export_csv.js
+++ test/test.export_csv.js
@@ -2,18 +2,25 @@ var assert = require('assert'),
   commasep = require('./../responses.js').commasep;
   
 suite('csvExport', function(){
-  test('commasep should turn a simple array into a csv string', function(){
-    
-    var row = ['a', 2, '3'];
-    var headers = ['first', 'second', 'third'];
-    var headerCount = {
-      'first': 1,
-      'second': 1,
-      'third': 1
-    };
-    
+  var row = ['a', 2, '3'];
+  var headers = ['first', 'second', 'third'];
+  var headerCount = {
+    'first': 1,
+    'second': 1,
+    'third': 1
+  };
+  var complexRow = ['a', [1,2,3], 4];
+  
+  test('commasep should turn a simple list into a csv string', function(){
     var csv = commasep(row, headers, headerCount);
     var expected = 'a,2,3';
+    assert.equal(csv,expected);
+  });
+  
+  
+  test('arrays should be serialized with semicolons', function() {
+    var csv = commasep(complexRow, headers, headerCount);
+    var expected = 'a,1;2;3,4';
     assert.equal(csv,expected);
   });
   
