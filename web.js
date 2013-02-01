09ebf4b0fdfe05f9ff9980911c4f37e4685119a0
Prashant Singh <prashant@codeforamerica.org>
2013-02-01 00:06:48 -0800

    Use native BSON parser

--- web.js
+++ web.js
@@ -328,7 +328,8 @@ function run(settings, cb) {
                                                           settings.mongo_port,
                                                           { auto_reconnect: true }), {
       w: 1,
-      safe: true
+      safe: true,
+      native_parser: true
     });
     setupRoutes(db, settings);
   }
