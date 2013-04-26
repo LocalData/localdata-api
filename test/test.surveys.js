6f58e84e270eb3f27c9f263320e655bc9629176a
Matt Hampel <matthew.hampel@gmail.com>
2013-04-25 21:16:19 -0400

    Make sure you can't save arbitrary fields

--- test/test.surveys.js
+++ test/test.surveys.js
@@ -82,6 +82,7 @@ suite('Surveys', function () {
       "name": "Test survey 1",
       "location": "Detroit",
       "type": "parcel",
+      "errantStuff": "foo",
       "paperinfo": {
         "dpi": 150,
         "regmarks": [
@@ -95,6 +96,7 @@ suite('Surveys', function () {
       "name": "Test survey 2",
       "users": ["2"],
       "type": "pointandparcel",
+      "errantStuff": 12345,
       "paperinfo": {
         "dpi": 150,
         "regmarks": [
@@ -206,6 +208,7 @@ suite('Surveys', function () {
           assert.deepEqual(data_two.surveys[i].paperinfo, body.surveys[i].paperinfo, 'Response differs from posted data');
           assert.equal(data_two.surveys[i].location, body.surveys[i].location, 'Response differs from posted data');
           assert.equal(data_two.surveys[i].type, body.surveys[i].type);
+          assert.notEqual(data_two.surveys[i].errantStuff, body.surveys[i].errantStuff);
 
           assert.notEqual(body.surveys[i].id, null, 'Response does not have an ID.');
 
