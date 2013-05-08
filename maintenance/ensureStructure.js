9c44ab748e6d642eed0921a5def46360af453059
Prashant Singh <prashant@codeforamerica.org>
2013-05-08 15:42:31 -0700

    Change ensureStructure maintenance script to use Mongoose

--- maintenance/ensureStructure.js
+++ maintenance/ensureStructure.js
@@ -10,8 +10,13 @@
  */
 'use strict';
 
-var mongo = require('mongodb');
-var makeSlug = require('slug');
+var makeSlug = require('slugs');
+var mongoose = require('mongoose');
+
+var Form = require('../lib/models/Form');
+var Response = require('../lib/models/Response');
+var Survey = require('../lib/models/Survey');
+var User = require('../lib/models/User');
 
 var settings;
 if (process.argv.length > 2) {
@@ -27,7 +32,7 @@ var FORMS = 'formCollection';
 var SURVEYS = 'surveyCollection';
 
 // Make sure an item has a unique slug
-function checkSlug(collection, name, index, done) {
+function checkSlug(name, index, done) {
   var slug = makeSlug(name);
   if (index > 0) {
     slug = slug + '-' + index;
@@ -35,16 +40,14 @@ function checkSlug(collection, name, index, done) {
 
   // See if we've already used this slug.
   // If we have, try another slug.
-  collection.find({ slug: slug }, function (err, cursor) {
+  Survey.count({ slug: slug }, function (err, count) {
     if (err) { return done(err); }
-    cursor.count(function (err, count) {
-      if (err) { return done(err); }
-      if (count > 0) {
-        checkSlug(collection, name, index + 1, done);
-      } else {
-        done(null, slug);
-      }
-    });
+
+    if (count > 0) {
+      checkSlug(name, index + 1, done);
+    } else {
+      done(null, slug);
+    }
   });
 }
 
@@ -70,137 +73,88 @@ function ensureStructure(db, callback) {
 
   // Make sure our collections are in good working order.
   // This primarily means making sure indexes are set up.
-  function ensureUsers(done) {
-    db.collection(USERS, function(error, collection) {
-      if (error) { return done(error); }
-
-      chain([function indexCreated(done) {
-        // Make sure email is unique
-          collection.ensureIndex({ "email": 1 }, { unique: true }, done);
-      }], done)();
-    });
-  }
-
-  function ensureResponses(done) {
-    db.collection(RESPONSES, function (error, collection) {
-      if (error) { return done(error); }
-      chain([function indexCentroid(done) {
-        // Ensure we have a geo index on the centroid field.
-        collection.ensureIndex({'geo_info.centroid': '2d'}, done);
-      },
-      function indexCreated(done) {
-        // Index the creation date, which we use to sort
-        collection.ensureIndex('created', done);
-      },
-      function indexSurvey(done) {
-        // Index the survey ID, which we use to look up sets of responses
-        collection.ensureIndex('survey', done);
-      },
-      function indexParcelId(done) {
-        // Index the parcel ID
-        collection.ensureIndex('parcel_id', done);
-      }], done)();
-    });
-  }
-
-  function ensureForms(done) {
-    db.collection(FORMS, function (error, collection) {
-      if (error) { return done(error); }
-      chain([function indexCreated(done) {
-        // Index the creation date, which we use to sort
-        collection.ensureIndex('created', done);
-      },
-      function indexParcelId(done) {
-        // Index the parcel IDs, used by paper forms
-        collection.ensureIndex('parcels.parcel_id', done);
-      }], done)();
-    });
-  }
-
-  function ensureSurveys(done) {
-    db.collection(SURVEYS, function (error, collection) {
-      if (error) { done(error); }
-      chain([function indexSlug(done) {
-        // Index the slug field.
-        collection.ensureIndex('slug', done);
-      },
-      function indexId(done) {
-        // Index the survey ID.
-        collection.ensureIndex('id', done);
-      }], done)();
-    });
-  }
 
   function ensureSlugs(done) {
-    db.collection(SURVEYS, function (error, collection) {
+    Survey.find({}).lean().exec(function (error, docs) {
+      // Reject if there's an error
+      if (error) {
+        return done(error);
+      }
 
-      // First, find all surveys.
-      collection.find({}, function(err, cursor) {
-        cursor.toArray(function (err, arr) {
+      // Look for surveys with no slug
+      // There are no surveys yet, so there's nothing to do.
+      if (docs.length === 0) {
+        return done(null);
+      }
 
-          // Reject if there's an error
-          if (err) { return done(err); }
-          var count = 0;
+      var count = 0;
 
-          // Look for surveys with no slug
-          // There are no surveys yet, so there's nothing to do.
-          if (arr.length === 0) {
-            return done();
-          }
+      docs.forEach(function (item) {
+        // Add a slug if there isn't one
+        if (item.slug === undefined) {
+          checkSlug(item.name, 0, function (error, slug) {
+            if (error) { return done(error); }
 
-          arr.forEach(function (item) {
-
-            // Add a slug if there isn't one
-            if (item.slug === undefined) {
-              checkSlug(collection, item.name, 0, function (err, slug) {
-                if (err) { return done(err); }
-
-                // Update entry
-                collection.update({_id: item._id}, {'$set': {slug: slug}}, function (error) {
-                  if (err) { return done(err); }
-                  count += 1;
-                  if (count === arr.length) {
-                    done();
-                  }
-                });
-              });
-            } else {
+            // Update entry
+            Survey.udpate({ _id: item._id }, { '$set': { slug: slug } }, function (error) {
+              if (error) { return done(error); }
               count += 1;
-              if (count === arr.length) {
-                done();
+              if (count === docs.length) {
+                done(null);
               }
-            }
+            });
           });
-        });
+        } else {
+          count += 1;
+          if (count === docs.length) {
+            done();
+          }
+        }
       });
     });
   }
+
+  // Wrap the calls in closures, so we can chain them.
+  function ensureUser(error) { User.ensureIndexes(error); }
+  function ensureResponse(error) { Response.ensureIndexes(error); }
+  function ensureForm(error) { Form.ensureIndexes(error); }
+  function ensureSurvey(error) { Survey.ensureIndexes(error); }
   
   // Chain everything together
-  chain([ensureUsers, ensureResponses, ensureForms, ensureSurveys, ensureSlugs], callback)();
+  chain([
+    ensureUser,
+    ensureResponse,
+    ensureForm,
+    ensureSurvey,
+    ensureSlugs
+  ], callback)();
 }
 
-var db = new mongo.Db(settings.mongo_db,
-                      new mongo.Server(settings.mongo_host,
-                                       settings.mongo_port,
-                                       { auto_reconnect: true }),
-                      { w: 1,
-                        safe: true,
-                        native_parser: settings.mongo_native_parser
+var opts = {
+  db: {
+    w: 1,
+    safe: true,
+    native_parser: settings.mongo_native_parser
+  }
+};
+
+if (settings.mongo_user !== undefined) {
+  opts.user = settings.mongo_user;
+  opts.pass = settings.mongo_password;
+}
+
+mongoose.connect(settings.mongo_host, settings.mongo_db, settings.mongo_port, opts);
+
+var db = mongoose.connection;
+
+db.on('error', function (error) {
+  console.log('Error connecting to mongo server.');
+  console.log(error);
+  throw error;
 });
 
-db.open(function() {
-  function close() { db.close(); }
-  if (settings.mongo_user !== undefined) {
-    db.authenticate(settings.mongo_user, settings.mongo_password, function(err, result) {
-      if (err) {
-        console.log(err.message);
-        db.close();
-        return;
-      }
-      ensureStructure(db, close);
-    });
-  } else {
-    ensureStructure(db, close);
-  }
+db.once('open', function () {
+  ensureStructure(db, function () {
+    db.close();
+  });
 });
