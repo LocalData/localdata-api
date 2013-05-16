fac96f7caf7a636ac1b6e08e11c029cf21d4ec4e
Prashant Singh <prashant@codeforamerica.org>
2013-05-16 10:59:29 -0700

    Add a string->float maintenance script

---
+++ maintenance/float-coords.js
@@ -0,0 +1,120 @@
+/*jslint node: true */
+
+/*
+ * Maintenance script to ensure coordinates are floats and not strings.
+ * 
+ * Usage:
+ * $ node float-coords.js
+ * $ node float-coords.js settings-file.js
+ *
+ * You can find the number of Polygons that have string coordinates in the mongo shell with
+ *   db.responseCollection.find({"geo_info.geometry.coordinates.0.0.0": {$type: 2}}).count()
+ * And MultiPolygons with
+ *   db.responseCollection.find({"geo_info.geometry.coordinates.0.0.0.0": {$type: 2}}).count()
+ *
+ */
+'use strict';
+
+var mongoose = require('mongoose');
+
+var Response = require('../lib/models/Response');
+
+var settings;
+if (process.argv.length > 2) {
+  settings = require(process.cwd() + '/' + process.argv[2]);
+} else {
+  settings = require('../settings-test.js');
+}
+
+function fixGeometries(done) {
+  var select = 'geo_info.geometry.coordinates';
+
+  function processStream(stream, work, done) {
+    stream
+    .on('data', function (doc) {
+      work(doc);
+    })
+    .on('error', done)
+    .on('close', function () {
+      done(null);
+    });
+  }
+
+  var polygonCount = 0;
+  function polygon(done) {
+    console.log('Fixing polygons');
+    var query = {
+      'geo_info.geometry.coordinates.0.0.0': {$type: 2}
+    };
+    processStream(Response.find(query).select(select).lean().stream(), function (doc) {
+      var points = doc.geo_info.geometry.coordinates[0];
+      var i;
+      var len = points.length;
+      for (i = 0; i < len; i += 1) {
+        points[0] = parseFloat(points[0]);
+        points[1] = parseFloat(points[1]);
+      }
+      Response.update({ _id: doc._id }, { $set: { 'geo_info.geometry.coordinates': points } }).exec();
+      polygonCount += 1;
+    }, done);
+  }
+
+  var multiPolygonCount = 0;
+  function multiPolygon(done) {
+    var query = {
+      'geo_info.geometry.coordinates.0.0.0.0': {$type: 2}
+    };
+    processStream(Response.find(query).select(select).lean().stream(), function (doc) {
+      var points = doc.geo_info.geometry.coordinates[0][0];
+      var i;
+      var len = points.length;
+      for (i = 0; i < len; i += 1) {
+        points[0] = parseFloat(points[0]);
+        points[1] = parseFloat(points[1]);
+      }
+      Response.update({ _id: doc._id }, { $set: { 'geo_info.geometry.coordinates': points } }).exec();
+      multiPolygonCount += 1;
+    }, done);
+  }
+
+  polygon(function (error) {
+    console.log('Fixed ' + polygonCount + ' Polygon geometries.');
+    if (error) {
+      console.log(error);
+      return;
+    }
+    multiPolygon(function (error) {
+      console.log('Fixed ' + multiPolygonCount + ' MultiPolygon geometries.');
+      done(error);
+    });
+  });
+}
+
+var opts = {
+  db: {
+    w: 1,
+    safe: true,
+    native_parser: settings.mongo_native_parser
+  }
+};
+
+if (settings.mongo_user !== undefined) {
+  opts.user = settings.mongo_user;
+  opts.pass = settings.mongo_password;
+}
+
+mongoose.connect(settings.mongo_host, settings.mongo_db, settings.mongo_port, opts);
+
+var db = mongoose.connection;
+
+db.on('error', function (error) {
+  console.log('Error connecting to mongo server.');
+  console.log(error);
+  throw error;
+});
+
+db.once('open', function () {
+  fixGeometries(function () {
+    db.close();
+  });
+});
