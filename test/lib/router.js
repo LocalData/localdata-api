1ca5708425cb6a07d1cc94136e5857b3f0f948ea
Prashant Singh <prashant@codeforamerica.org>
2013-05-24 13:57:30 -0700

    Add a router for HTTPS to simulate Heroku

---
+++ test/lib/router.js
@@ -0,0 +1,45 @@
+/*jslint node: true */
+'use strict';
+
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var httpProxy = require('http-proxy');
+
+var settings = require('../../settings-test');
+var server = require('../../lib/server');
+
+var key = fs.readFileSync(__dirname + '/../data/test-key.pem', 'utf8');
+var cert = fs.readFileSync(__dirname + '/../data/test-cert.pem', 'utf8');
+
+var proxy = new httpProxy.HttpProxy({
+  target: {
+    host: 'localhost', 
+    port: settings.port
+  }
+});
+
+var router = 
+module.exports = {
+  run: function start(settings, done) {
+    // Start the real server.
+    server.run(settings, function (error) {
+      if (error) { return done(error); }
+
+      // Start the HTTPS router.
+      router = https.createServer({
+        key: key,
+        cert: cert
+      }, function (req, res) {
+        proxy.proxyRequest(req, res);
+      }).listen(settings.testSecurePort, function (error) {
+        console.log('Listening for HTTPS on ' + settings.testSecurePort);
+        done(error);
+      });
+    });
+  },
+  stop: function stop(done) {
+    router.close();
+    server.stop(done);
+  }
+}
