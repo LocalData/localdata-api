e3406f306fc645d69f944656618b2e4c23517da6
Prashant Singh <prashant@localdata.com>
2015-01-22 18:43:49 -0800

    Use a MongoDB connection string

--- settings.js
+++ settings.js
@@ -14,12 +14,7 @@ settings.email.from = 'LocalData <support@localdata.com>';
 settings.email.to = process.env.TEST_EMAIL || 'LocalData <support@localdata.com>'; // Send test emails to your address!
 
 // MongoDB
-settings.mongo_host = process.env.MONGO_HOST || 'localhost';
-settings.mongo_port = parseInt(process.env.MONGO_PORT, 10);
-if (isNaN(settings.mongo_port)) { settings.mongo_port = 27017; }
-settings.mongo_db = process.env.MONGO_DB || 'scratchdb';
-settings.mongo_user = process.env.MONGO_USER;
-settings.mongo_password = process.env.MONGO_PASSWORD;
+settings.mongo = process.env.MONGO;
 settings.mongo_native_parser = false;
 if (process.env.MONGO_NATIVE_PARSER !== undefined && process.env.MONGO_NATIVE_PARSER.toLowerCase() === 'true') {
   settings.mongo_native_parser = true;
